3370ab519baab5a58bffe7dc07f74d3e0866c080
crevitch <crevitch@gmail.com>
2015-08-28 18:10:57 -0400

    back to 10

--- views/js/maina.js
+++ views/js/maina.js
@@ -532,7 +532,7 @@ window.addEventListener('scroll', updatePositions);
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
-  for (var i = 0; i < 32; i++) {	//changed from 200
+  for (var i = 0; i < 10; i++) {	//changed from 200
     var elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza.png";
